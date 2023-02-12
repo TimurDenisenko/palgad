9a9f1ca44f1a7849c05ed3422e4cbc5ba051f8e3
TimurDenisenko <timur.denisenko.2887@gmail.com>
2023-02-13 01:57:01 +0200

    palgad2

--- palk.py
+++ palk.py
@@ -8,9 +8,9 @@ def limed(x:list,y:list):
     while n.isdigit()==False:
         n=input("Kirjuta õige arv! ")
     for i in range(int(n)):
-        nimi=input("Mis nimi on uue inimene? ").title()
+        nimi=input("Mis nimi on uue inimene? ")
         while nimi.isdigit() or len(nimi)<2:
-            nimi=input("Kirjuta õige nimi ").title()
+            nimi=input("Kirjuta õige nimi ")
 
         palk=input("Kui palju teda maksab? ")
         while palk.replace(".","",1).isdigit()==False or palk==0:
@@ -35,20 +35,20 @@ def kip(x:list,y:list):
     nimi=input("Kirjuta nimi mis tahad kustuda ").title()
     while nimi.isdigit() or len(nimi)<2 or nimi not in x:
         nimi=input("Kirjuta õige nimi! ").title()
-    n=x.count(nimi)
-    if n!=0:
+    j=x.count(nimi)
+    if j!=1:
         print("Siin on mõned inimesed sama nimega")
         valik=input("Kas sa tahad kustuda mõned inimesed? (jah või ei) ").lower()
         while valik not in ["jah","ei"]:
             valik=input("Kirjuta ainult jah või ei ").lower() 
         if valik=="jah":
             j=input("Kui palju inimesed sama nimega sa tahad kustuda? ")
-            while j.isdigit()==False or int(j)>=n+1 or int(j)==1:
+            while j.isdigit()==False or int(j)>=int(j)+1 or int(j)==1:
                 j=input("Kirjuta õige arv! ")
             j=int(j)
             for _ in range(j):
-                valik=input(f"Siin on {n} nimed, mis nimed sa tahad kustuda? ")
-                while valik.isdigit()==False or int(valik)>=n+1:
+                valik=input(f"Siin on {j} nimed, mis nimed sa tahad kustuda? ")
+                while valik.isdigit()==False or int(valik)>=j+1:
                     valik=input("Kirjuta õige arv! ")
                     kopia=x.copy()
                 for i in range(int(valik)):
@@ -62,14 +62,14 @@ def kip(x:list,y:list):
                         x.insert(ind,"")
                         y.pop(ind)
                         y.insert(ind,"")
-                j=0
+            j=0
             o=x.count("")
             for e in range(o):
                 x.remove("")
                 y.remove("")
         else:
-            tegevus=input(f"Siin on {n} nimed, mis nimi sa tahad kustuda? ")
-            while tegevus.isdigit()==False or int(tegevus)>=n+1:
+            tegevus=input(f"Siin on {j} nimed, mis nimi sa tahad kustuda? ")
+            while tegevus.isdigit()==False or int(tegevus)>=j+1:
                 tegevus=input("Kirjuta õige arv! ") 
             for _ in range(int(tegevus)-1):
                 k=kopia.index(nimi)
@@ -92,7 +92,7 @@ def maks(x:list,y:list):
     pm=[]
     ind=y.index(max(y))
     n=y.count(y[ind])
-    if n>1:
+    if n!=1:
         print("Siin on mõned inimesed kes saab maksimaalne palk")
         kopia=y.copy()
         print("Saavad saama palk: ", end="")
@@ -123,9 +123,190 @@ def mini(x:list,y:list):
         for i in range(n):
             print(f"{x[ind]}, ", end="") 
             kopia.pop(ind) 
-            kopia.insert(ind,0)
+            kopia.insert(ind,(min(y)+1))
             ind=kopia.index(min(kopia))
         vas=(f"ja nad saavad {min(y)}")
     else:
         vas=(f"Saab minimaalne palka {x[ind]} ja ta saab {y[ind]}")
     return vas
+
+def saapa(x:list,y:list):
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    a=0
+    for u in range(len(y)):
+        for i in range(u+1,len(y)):
+            if y[u]==y[i]:
+                ind=y.index(y[i])
+                print(f"{x[u]} ja {x[i]} saavad saama palk, see on {y[u]} euro.")
+                z=1
+    if z==0:
+        print(f"Sul ei ole inimesed kes saavad saama palk.")
+
+def lepani(x:list,y:list): 
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    nimi=input("Kelle palk tahad leia? ").title()
+    while nimi not in x:
+        nimi=input("Palun kirjuta õige nimi ").title()
+    n=x.count(nimi)
+    if n!=1:
+        print(f"Siin on mõned inimesed kes nimi on {nimi}") 
+        kopia=x.copy()
+        for i in range(n):
+            ind=kopia.index(nimi)
+            kopia.remove(nimi)
+            kopia.insert(ind,"")
+            print(f"{i+1} {nimi} saab {y[ind]}")
+    else:
+        ind=x.index(nimi)
+        print(f"{nimi} saab {y[ind]}")
+
+def suva(x:list,y:list): 
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    palk=input("Kirjuta igasugune palk ")
+    while palk.replace(".","",1).isdigit()==False: 
+        palk=input("Kirjuta õige palk ")
+    palk=float(palk)
+    for i in range(len(x)):
+        if y[i]>palk: 
+            print(f"{x[i]} saab suurem kui {palk}")
+        elif y[i]<palk: 
+            print(f"{x[i]} saab väiksem kui {palk}")
+        else:
+            print(f"{x[i]} saab täpselt {palk}")
+
+def tomami(x:list,y:list): 
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    kopia=y.copy()
+    for i in range(3):
+        ind=kopia.index(min(kopia))
+        print(f"{i+1} inimene - {x[ind]} saab väikse palk: {y[ind]}")
+        kopia.pop(ind)
+        kopia.insert(ind,max(y)+1)
+    kopia=y.copy()
+    for i in range(3):
+        ind=kopia.index(max(kopia))
+        print(f"{i+1} inimene - {x[ind]} saab suur palk: {y[ind]}")
+        kopia.pop(ind)
+        kopia.insert(ind,min(y)+1)
+
+def keskmine(x:list,y:list): 
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    kesk=sum(y)/len(y)
+    print(f"Keskmine palk on {kesk}")
+    for i in range(len(x)):
+        if y[i]>=kesk:
+            print(f"{x[i]} saab suurem kui keskmine palk, ta saab {y[i]}")
+
+def tulumaks(x:list,y:list): 
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    for i in range(0,len(y)):
+        if y[i]<500:
+            palk=y[i]
+        elif 500>=y[i]<=1200:
+            palk=(y[i]-500)-(y[i]-500)*0.2
+        elif 1200>y[i]>=2099:
+            palk=(500-(5/9)*(y[i]-1200))-(500-(5/9)*(y[i]-1200))*0.2
+        else:
+            palk=y[i]*0.2
+        print(f"{x[i]} on maksuvaba palk {palk}")
+
+def sorteerimine(x:list,y:list):
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype: list, list
+    """
+    vali=input("Sorteeri nime (1) või palga järgi (2) ")
+    while vali not in ["1","2"]:
+        vali=input("Kirjuta ainult 1 või 2 ")
+    if vali=="1":
+        vali_2=input("A–Z või Z–A ").upper()
+        while vali_2 not in ["A-Z","Z-A"]:
+            vali_2=input("A–Z või Z–A ").upper()
+        for p in range(0,len(x)):
+            for o in range(0,len(x)):
+                if x[p]==x[o] and p!=o:
+                    for u in range(0,len(x)):
+                        x[o]+=f"_{u+1}"
+                        break
+        kopia=x.copy()
+        x.sort()
+        y1=[]
+        for i in range(0,len(x)):
+            ind=kopia.index(x[i])
+            y1.insert(i,y[ind])
+        y=y1
+        if vali_2=="Z-A":
+            x.reverse()
+            y.reverse()
+    else:
+        for p in range(0,len(x)):
+            for o in range(0,len(x)):
+                if y[p]<y[o]:
+                    abi=y[p]
+                    y[p]=y[o]
+                    y[o]=abi
+                    abi=x[p]
+                    x[p]=x[o]
+                    x[o]=abi
+        vali_2=input("Kasvav või kahanev järjekord ").title()
+        while vali_2 not in ["Kasvav","Kahanev"]:
+            vali_2=input("Kasvav või kahanev! ").title()
+        if vali_2=="Kahanev":
+            x.reverse()
+            y.reverse()
+    return x,y 
+
+def keskpop(x:list,y:list):
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype: list, list
+    """
+    x1=[] 
+    y1=[]
+    kesk=sum(y)/len(y)
+    print(f"Keskmine palk on {kesk}")
+    for i in range(0,len(x)):
+        if y[i]>kesk:
+            y1.append(y[i])
+            x1.append(x[i])
+    x=x1 
+    y=y1
+    return x,y
+
+def tint(x:list,y:list):
+    """Kirjeldus...
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype: list, list
+    """
+    for i in range(0,len(x)):
+        x[i]=x[i].title()
+        y[i]=round(y[i],1) 
+        y[i]=int(y[i])
+    return x,y
